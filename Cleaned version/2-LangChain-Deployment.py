e6d886ec4197fa459c1bd8bb8a913db0b889114b
zhenyu <freemensword@gmail.com>
2025-11-12 02:48:25 +0100

    2-LangChain 完成

---
+++ Cleaned version/2-LangChain-Deployment.py
@@ -0,0 +1,76 @@
+python -m pip install --upgrade pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install modelscope==1.16.1
+pip install langchain==0.2.3
+pip install transformers==4.43.2
+pip install accelerate==0.32.1
+
+from modelscope import snapshot_download
+from langchain.llms.base import LLM
+from typing import Any, List, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, LlamaTokenizerFast
+import torch
+
+model_dir = snapshot_download('qwen/Qwen1.5-7B-Chat', 
+                              cache_dir='/root/autodl-tmp', 
+                              revision='master')
+
+
+class Qwen1_5_LLM(LLM):
+
+    def __init__(self, model_name_or_path: str):
+
+        super().__init__()
+        print("Loading Model.")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast = False)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path, 
+            torch_dtype = torch.bfloat16, 
+            device_map = "auto"
+        )
+        self.model.generation_config = GenerationConfig.from_pretrained(model_name_or_path)
+
+    def _call(self, prompt):
+
+        message = [
+            {"role": "system", "content": "you always help me solve problems."},
+            {"role": "user", "content": prompt}
+        ]
+
+        input_text = self.tokenizer.apply_chat_template(
+            message,
+            tokenize = False,
+            add_generation_prompt = True
+        )
+
+        input_id_mask_dict = self.tokenizer(
+            [input_text],
+            return_tensors = "pt"
+        ).to('cuda')
+
+        input_ids = input_id_mask_dict.input_ids
+        input_and_generated_ids = self.model.generate(
+            input_ids,
+            max_new_tokens = 512
+        )
+
+        generated_ids = [all_tokens_ids[len(input_ids):] for all_tokens_ids, input_ids in zip(input_and_generated_ids, input_ids)]
+        response = self.tokenizer.batch_decode(
+            generated_ids,
+            skip_special_tokens = True
+        )[0]
+        
+        return response
+
+    @property
+    def _llm_type(self) -> str:
+        return "Qwen_5_LLM"
+
+if __name__ ==  "__main__":
+    llm = Qwen1_5_LLM(mode_name_or_path = "/root/autodl-tmp/qwen/Qwen1.5-7B-Chat")
+    llm("你是谁")
+
+
+
